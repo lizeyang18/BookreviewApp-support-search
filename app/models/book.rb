0a89cf6984ad7f780a575d9e570ee675817a3d4e
Nguyen Minh Duc <nmduc3@gmail.com>
2018-03-26 17:16:48 +0700

    make a search by name

--- app/models/book.rb
+++ app/models/book.rb
@@ -4,4 +4,6 @@ class Book < ApplicationRecord
   has_many :reviews
   has_attached_file :book_img, styles: { book_index: "250x350>", book_show: "325x475>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :book_img, content_type: /\Aimage\/.*\z/
+
+  scope :title_like, ->(title){where "title LIKE ?", "%#{title}%"}
 end
