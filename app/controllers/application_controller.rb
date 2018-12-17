3facd95ca65c4fd57863ccc9ab6469e61f412e19
lizeyang <1026609843@qq.com>
2018-12-17 07:36:54 +0000

    changeadmin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  def is_admin?
-    unless current_user.admin?
-      flash[:danger] = "Oops!! You don't have privilege to to this action"
-      redirect_to root_path
-    end
-  end
+ 
+ #def is_admin?
+   # unless current_user.admin?
+     # flash[:danger] = "Oops!! You don't have privilege to to this action"
+    #  redirect_to root_path
+    #end
+  #end
 end
